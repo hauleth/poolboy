9272f05d75e8130111f54aa8bb990a3df52b317c
Łukasz Niemier <~@hauleth.dev>
2026-03-30 11:28:26 +0200

    Reformat codebase

--- src/poolboy_top_sup.erl
+++ src/poolboy_top_sup.erl
@@ -16,21 +16,31 @@ start_link(PoolArgs, WorkerArgs) ->
 init({PoolArgs, WorkerArgs}) ->
     Mod = proplists:get_value(worker_module, PoolArgs),
     Name = proplists:get_value(name, PoolArgs),
-    {ok, {#{strategy => one_for_all,
-             intensity => 0,
-             period => 1,
-             auto_shutdown => any_significant}, [
-                #{id => poolboy_sup,
-                  start => {poolboy_sup, start_link, [Mod, WorkerArgs]},
-                  restart => permanent,
-                  shutdown => infinity,
-                  type => supervisor,
-                  modules => [poolboy_sup]},
-                #{id => poolboy,
-                  start => {poolboy, start_link_worker, [Name, PoolArgs]},
-                  restart => transient,
-                  significant => true,
-                  shutdown => 5000,
-                  type => worker,
-                  modules => [poolboy]}
-            ]}}.
+    {ok,
+        {
+            #{
+                strategy => one_for_all,
+                intensity => 0,
+                period => 1,
+                auto_shutdown => any_significant
+            },
+            [
+                #{
+                    id => poolboy_sup,
+                    start => {poolboy_sup, start_link, [Mod, WorkerArgs]},
+                    restart => permanent,
+                    shutdown => infinity,
+                    type => supervisor,
+                    modules => [poolboy_sup]
+                },
+                #{
+                    id => poolboy,
+                    start => {poolboy, start_link_worker, [Name, PoolArgs]},
+                    restart => transient,
+                    significant => true,
+                    shutdown => 5000,
+                    type => worker,
+                    modules => [poolboy]
+                }
+            ]
+        }}.
--- src/poolboy_worker.erl
+++ src/poolboy_worker.erl
@@ -2,9 +2,11 @@
 
 -module(poolboy_worker).
 
--callback start_link(WorkerArgs) -> {ok, Pid} |
-                                    {error, {already_started, Pid}} |
-                                    {error, Reason} when
+-callback start_link(WorkerArgs) ->
+    {ok, Pid}
+    | {error, {already_started, Pid}}
+    | {error, Reason}
+when
     WorkerArgs :: proplists:proplist(),
-    Pid        :: pid(),
-    Reason     :: term().
+    Pid :: pid(),
+    Reason :: term().
